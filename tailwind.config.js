0eb298b46d6d22ef9a65a310c2e7a885203737d1
unknown <ronaldo.mendoza.60098@usalesiana.edu.bo>
2024-10-08 03:06:18 -0400

    feat: implement prettier-vite-eslint-tailwind and remove carpets react

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      primary: '#BB3D0B',
+      secondary: '#131313',
+      success: '#00C247',
+      info: '#004CE8',
+      warning: '#FFE16A',
+      error: '#FF3333',
+      disabled: '#D8D8D8'
+    }
+  }
+};
