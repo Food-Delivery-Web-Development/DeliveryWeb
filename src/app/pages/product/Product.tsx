c5e58042148d452de4484110686b1337aa7eb0dd
Leonardo Herrera <leonardo.herrera@fundacion-jala.org>
2024-10-09 12:06:33 -0400

    feat: rendering images

--- src/app/pages/product/Product.tsx
+++ src/app/pages/product/Product.tsx
@@ -5,34 +5,52 @@ import { Title } from "../../shared-components/Title";
 
 function Product() {
   const { id } = useParams<{ id: string }>();
-
   const { product } = useGetProduct(id);
 
-  if(!product){
-    return <Loading />
+  if (!product) {
+    return <Loading />;
   }
 
   return (
     <div className="flex justify-center items-center mt-10 text-white">
-      <h2 className="max-w-4xl w-full">
+      <div className="max-w-4xl w-full">
         <h2 className="text-4xl mb-4 text-primary">
           <Title>{product.name}</Title>
         </h2>
-        <div className="relative bg-gray-300 w-full h-56 mb-4 rounded-lg">
-          <div className="absolute bottom-4 right-4 flex items-center space-x-4">
+
+        <div className="relative w-full h-56 mb-4 rounded-lg">
+          {product.imageUrl ? (
+            <img
+              src={product.imageUrl}
+              alt={product.name}
+              className="w-full h-full object-cover rounded-lg"
+            />
+          ) : (
+            <div className="bg-gray-300 w-full h-full rounded-lg"></div>
+          )}
+        </div>
+
+        
+
+        <div className="flex items-center justify-between space-x-4 mt-4">
+          <div>
+            <h3 className="text-xl font-semibold mb-2">Description</h3>
+          </div>
+
+          <div className="flex items-center justify-center">
             <span className="bg-gray-800 text-white px-3 py-1 rounded-md text-sm">
               30-45 min
             </span>
             <div className="flex items-center">
-              <span className="text-xl font-semibold ml-1 text-primary">★ 4.5</span>
+              <span className="text-xl font-semibold ml-1 text-primary">
+                ★ {product.rating || "0.0"}
+              </span>
             </div>
           </div>
         </div>
-        <h3 className="text-xl font-semibold mb-2">Description</h3>
-        <p className="text-gray-400">
-          {product.description}
-        </p>
-      </h2>
+
+        <p className="text-gray-400">{product.description}</p>
+      </div>
     </div>
   );
 }
